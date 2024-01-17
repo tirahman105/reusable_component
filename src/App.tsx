fe0eb1e8dd3512c6691197801292fcc3bd7135d6
tirahman105 <tirahman105@gmail.com>
2024-01-17 22:56:57 +0600

    Resolve Rest Params, Refs, And TypeSript Errors, BTN Component fomplete

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <Container>
       <div className="h-screen w-full flex justify-center items-center">
         <div className="w-96 border border-slate-600 p-10 rounded">
-          <Button varient="outline" className="w-full"></Button>
+          <Button className="w-full"></Button>
         </div>
       </div>
     </Container>
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,19 +1,42 @@
 import { twMerge } from "tailwind-merge";
 import { clsx } from "clsx";
 import cn from "../../utils/cn";
+import { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef } from "react";
 
-const Button = ({ className, varient }) => {
-  const getVarient = (varient) => {
-    switch (varient) {
-      case "outline":
-        return "btn-outline";
-      case "ghost":
-        return "btn-ghost";
-      default:
-        return "btn-solid";
-    }
-  };
-  return <button className={cn(getVarient(varient), className)}>Click</button>;
+type TRef = HTMLButtonElement;
+type TVariant = "solid" | "ghost" | "outline";
+
+type TButtonOptions = {
+  variant?: TVariant;
 };
+type TButton = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  TButtonOptions;
+
+const Button = forwardRef<TRef, TButton>(
+  ({ className, variant = "solid", ...rest }, ref) => {
+    const getVarient = (variant: TVariant) => {
+      switch (variant) {
+        case "outline":
+          return "btn-outline";
+        case "ghost":
+          return "btn-ghost";
+        default:
+          return "btn-solid";
+      }
+    };
+    return (
+      <button
+        {...rest}
+        ref={ref}
+        className={cn(getVarient(variant), className)}
+      >
+        Click
+      </button>
+    );
+  }
+);
 
 export default Button;
