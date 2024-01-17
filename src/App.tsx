99afed8160571e87b066c01ba72af826cbc5d0cb
tirahman105 <tirahman105@gmail.com>
2024-01-17 20:06:59 +0600

    reusable container component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import MainLayout from "./components/layout/MainLayout";
+import Container from "./components/ui/Container";
 
 function App() {
-  return <MainLayout></MainLayout>;
+  return (
+    <Container>
+      <h1>This is main </h1>
+    </Container>
+  );
 }
 
 export default App;
