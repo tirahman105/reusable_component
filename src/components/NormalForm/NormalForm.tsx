6f0e5fbb3e1248ef0af6145e8c8e34372fa9dcbb
tirahman105 <tirahman105@gmail.com>
2024-01-25 13:27:49 +0600

    form provider and watch

--- src/components/NormalForm/NormalForm.tsx
+++ src/components/NormalForm/NormalForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { FieldValues, useForm, FormProvider } from "react-hook-form";
 import cn from "../../utils/cn";
 import Button from "../ui/Button";
 
@@ -7,82 +7,92 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { SignUpSchema, TNormalForm } from "./validation";
 
 const NormalForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<TNormalForm>({
+  const methods = useForm<TNormalForm>({
     resolver: zodResolver(SignUpSchema),
   });
+
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
   };
+  console.log(watch("name"));
 
   const double = true;
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className={cn(
-        "border border-gray-400 rounded-lg shadow-lg max-w-5xl p-5 mx-auto",
-        {
-          "max-w-5xl": double,
-          "max-w-md": !double,
-        }
-      )}
-    >
-      <div
-        className={cn("border grid grid-cols-1 justify-items-center  gap-5", {
-          "md:grid-cols-2": double,
-        })}
+    <FormProvider {...methods}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={cn(
+          "border border-gray-400 rounded-lg shadow-lg max-w-5xl p-5 mx-auto",
+          {
+            "max-w-5xl": double,
+            "max-w-md": !double,
+          }
+        )}
       >
-        <div className="w-full max-w-md">
-          <label className="block" htmlFor="name">
-            Name
-          </label>
-          <input type="text" id="name" {...register("name")} />
-          {errors.name && (
-            <span className="text-sm text-red-500">{errors.name.message}</span>
-          )}
-        </div>
-        <div className="w-full  max-w-md">
-          <label className="block" htmlFor="email">
-            Email
-          </label>
-          <input type="email" id="email" {...register("email")} />
-          {errors.email && (
-            <span className="text-sm text-red-500">{errors.email.message}</span>
-          )}
-        </div>
-        <div className="w-full  max-w-md">
-          <label className="block" htmlFor="password">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            {...register("password", { minLength: 8 })}
-          />
-          {errors.password && (
-            <span className="text-sm text-red-500">
-              {errors.password.message}
-            </span>
-          )}
-        </div>
-
         <div
-          className={cn("border grid-cols-1  grid  gap-5 py-8", {
-            "md:grid-cols-2  ": double,
+          className={cn("border grid grid-cols-1 justify-items-center  gap-5", {
+            "md:grid-cols-2": double,
           })}
         >
-          <div className="w-full max-w-md col-start-1 md:col-start-2 flex justify-end bg-red-600 ">
-            <Button className="w-full md:w-fit" type="submit">
-              Submit
-            </Button>
+          <div className="w-full max-w-md">
+            <label className="block" htmlFor="name">
+              Name
+            </label>
+            <input type="text" id="name" {...register("name")} />
+            {errors.name && (
+              <span className="text-sm text-red-500">
+                {errors.name.message}
+              </span>
+            )}
+          </div>
+          <div className="w-full  max-w-md">
+            <label className="block" htmlFor="email">
+              Email
+            </label>
+            <input type="email" id="email" {...register("email")} />
+            {errors.email && (
+              <span className="text-sm text-red-500">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+          <div className="w-full  max-w-md">
+            <label className="block" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              {...register("password", { minLength: 8 })}
+            />
+            {errors.password && (
+              <span className="text-sm text-red-500">
+                {errors.password.message}
+              </span>
+            )}
+          </div>
+
+          <div
+            className={cn("border grid-cols-1  grid  gap-5 py-8", {
+              "md:grid-cols-2  ": double,
+            })}
+          >
+            <div className="w-full max-w-md col-start-1 md:col-start-2 flex justify-end bg-red-600 ">
+              <Button className="w-full md:w-fit" type="submit">
+                Submit
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </FormProvider>
   );
 };
 
